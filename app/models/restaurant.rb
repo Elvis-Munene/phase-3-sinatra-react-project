ee833159c963a5d8a52c8724dc681fce9faadc5d
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-09-11 09:33:10 +0300

    adds data to restaurants and reviews

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,7 +4,9 @@ has_many :reviews
     def slugify
     self.slug = name.parameterize
     end
-    def avg_score
-    reviews.average(:score).round(2).to_f
-    end
+
+    # def avg_score
+    # reviews.average(:score).round(2).to_f
+    # end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,37 @@
 
 restaurants = Restaurant.create([
 {
-    name:
-    image_url:
+    name:"ole sereni",
+    image_url:"https://emara.ole-sereni.com/rooms-and-suites/?gclid=EAIaIQobChMItvSHg4mM-gIVrYxoCR2SkAWeEAAYASAAEgIS6PD_BwE"
+},
+{
+    name:"CJs",
+    image_url:"https://cjs.co.ke/"
+},
+{
+    name:"Art Cafe",
+    image_url:"https://www.artcaffe.co.ke/"
 }
 ])
 
 
 reviews = Review.create([
     {
-        title:
-        description:
-        score:
+        title:"Great restaurant",
+        description:"I had a wonderful time",
+        score:6,
+        restaurant: restaurants.first
+    },
+    {
+        title:"Bad restaurant",
+        description:"I had a difficult time",
+        score:1,
+        restaurant: restaurants.first
+    },
+    {
+        title:"Great restaurant",
+        description:"I loved the services",
+        score:4,
         restaurant: restaurants.first
     }
 ])
